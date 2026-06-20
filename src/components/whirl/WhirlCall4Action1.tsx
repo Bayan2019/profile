c4cbb199229ed36a33a39b8909ab2f4347b0340e
Bayan2019 <saparbayevabt@gmail.com>
2026-06-20 23:56:13 +0500

    Whirl -- Call for action

--- src/components/whirl/WhirlCall4Action1.tsx
+++ src/components/whirl/WhirlCall4Action1.tsx
@@ -1,6 +1,91 @@
+import { FaCheck } from "react-icons/fa";
+
 const WhirlCall4Action1 = () => {
     return (
-        <h1>Book a Demo</h1>
+        <section
+            style={{width:'1440px', height:'550px', 
+                backgroundColor:'#0070A0', padding:'auto'}}
+        >
+            <div
+                style={{width:'1130px', height:'389.96875', left:'155px', top:'80.39px',
+                    margin:'auto', display:'inline-flex', justifyContent:'space-between',
+                    // padding:'auto'
+                }}
+            >
+                <img src="/images/IMAGE-4.png"
+                    style={{width:'382px', height:'374px', top:'-0.39px', left:'60px',
+                        border:'none', borderRadius:'0px', marginTop:'78px',
+                    }}
+                 />
+                <div
+                    style={{width:'501px', height:'389px', top:'0.48px', left:'629px', 
+                        textAlign:'left'}}
+                >
+                    <p
+                        style={{ width:'403px', marginTop: '78px', lineHeight: '50.4px',
+                            fontSize:'42px', fontFamily:'Fraunces', color:'#FFFFFF'}}
+                    >
+                        Set, forget, and then track.
+                    </p>
+                    <p
+                        style={{width:'481px', height:'81px', fontSize:'18px', 
+                            color:'#FFFFFF', lineHeight:'27px', paddingRight:'5px'}}
+                    >
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    <div
+                        style={{width:'501px', height:'99px', top:'219px',
+                            paddingLeft:'0px'
+                        }}
+                    >
+                        <div
+                            style={{width:'501px', height:'21px', top:'0.13px',
+                                display:'inline-flex', margin:'6px'
+                            }}
+                        >
+                            <FaCheck style={{fontSize:'17px', color:'#FFCCDD'}} />
+                            <p
+                                style={{fontSize:'16px', lineHeight:'21.6px', letterSpacing:'0.2px', 
+                                    color:'#FFFFFF', marginLeft:'2px'}}
+                            >
+                                Understand your options
+                            </p>
+                        </div>
+                        <div
+                            style={{width:'501px', height:'21px', top:'0.13px',
+                                display:'inline-flex', margin:'6px'
+                            }}
+                        >
+                            <FaCheck style={{fontSize:'17px', color:'#FFCCDD'}} />
+                            <p
+                                style={{fontSize:'16px', lineHeight:'21.6px', letterSpacing:'0.2px', 
+                                    color:'#FFFFFF', marginLeft:'2px'}}
+                            >
+                                No lock-ins
+                            </p>
+                        </div>
+                        <div
+                            style={{width:'501px', height:'21px', top:'0.13px',
+                                display:'inline-flex', margin:'6px'
+                            }}
+                        >
+                            <FaCheck style={{fontSize:'17px', color:'#FFCCDD'}} />
+                            <p
+                                style={{fontSize:'16px', lineHeight:'21.6px', letterSpacing:'0.2px', 
+                                    color:'#FFFFFF', marginLeft:'2px'}}
+                            >
+                                You own the shares
+                            </p>
+                        </div>
+                    </div>
+
+                    <div style={{width:'176px', height:'43px', top:'334.13px', 
+                        color:'#FFFFFF', textAlign:'center', paddingTop:'16px'}}>
+                        Book a demo
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
