b1dc08420b0961d7506d38c37dc8b3797bbe36b7
Bayan2019 <saparbayevabt@gmail.com>
2026-06-21 02:43:16 +0500

    whirl

--- src/components/whirl/WhirlTasks.tsx
+++ src/components/whirl/WhirlTasks.tsx
@@ -1,3 +1,7 @@
+import { BsHandThumbsUp, BsLightning } from "react-icons/bs";
+import { FaExclamation } from "react-icons/fa";
+import { PiShareNetworkLight } from "react-icons/pi";
+
 const WhirlTasks = () => {
     return (
         <section
@@ -15,6 +19,78 @@ const WhirlTasks = () => {
                     >
                         Your tasks, automated.
                     </p>
+                    <div
+                        style={{width:'650px', height:'54px',
+                            fontSize:'18px', lineHeight:'27px', letterSpacing:'0.2px'}}
+                    >
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </div>
+                </div>
+                <div
+                    style={{width:'1130px', height:'218px', marginTop:'32px',
+                        display:'inline-flex', justifyContent:'space-between'
+                    }}
+                >
+                    <div
+                        style={{width:'234.5px', height:'218px', textAlign:'left'}}
+                    >
+                        <PiShareNetworkLight style={{fontSize:'55px', color:'#006B99'}} />
+                        <p
+                            style={{fontFamily:'Fraunces', fontSize:'25px', lineHeight:'31.25px', letterSpacing:'0.2px'}}
+                        >
+                            Learn your options.
+                        </p>
+                        <p
+                            style={{fontSize:'16px', lineHeight:'24px', letterSpacing:'0.2px'}}
+                        >
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.
+                        </p>
+                    </div>
+                    <div
+                        style={{width:'234.5px', height:'218px', textAlign:'left'}}
+                    >
+                        <BsLightning style={{fontSize:'55px', color:'#006B99'}} />
+                        <p
+                            style={{fontFamily:'Fraunces', fontSize:'25px', lineHeight:'31.25px', letterSpacing:'0.2px'}}
+                        >
+                            Stay informed.
+                        </p>
+                        <p
+                            style={{fontSize:'16px', lineHeight:'24px', letterSpacing:'0.2px'}}
+                        >
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et doloretro.
+                        </p>
+                    </div>
+                    <div
+                        style={{width:'234.5px', height:'218px', textAlign:'left'}}
+                    >
+                        <BsHandThumbsUp style={{fontSize:'55px', color:'#006B99'}} />
+                        <p
+                            style={{fontFamily:'Fraunces', fontSize:'25px', lineHeight:'31.25px', letterSpacing:'0.2px'}}
+                        >
+                            Automate it all.
+                        </p>
+                        <p
+                            style={{fontSize:'16px', lineHeight:'24px', letterSpacing:'0.2px'}}
+                        >
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ipsum.
+                        </p>
+                    </div>
+                    <div
+                        style={{width:'234.5px', height:'218px', textAlign:'left'}}
+                    >
+                        <FaExclamation style={{fontSize:'55px', color:'#006B99'}} />
+                        <p
+                            style={{fontFamily:'Fraunces', fontSize:'25px', lineHeight:'31.25px', letterSpacing:'0.2px'}}
+                        >
+                            Stay informed.
+                        </p>
+                        <p
+                            style={{fontSize:'16px', lineHeight:'24px', letterSpacing:'0.2px'}}
+                        >
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod incididunt ut labore et consectetur.
+                        </p>
+                    </div>
                 </div>
             </div>
         </section>
