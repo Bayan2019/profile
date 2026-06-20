3b244f69ff0eeac467e3d46839333d7cda8502a3
Bayan2019 <saparbayevabt@gmail.com>
2026-06-21 01:52:03 +0500

    whirl -- blogs

--- src/components/whirl/WhirlBlogs.tsx
+++ src/components/whirl/WhirlBlogs.tsx
@@ -1,6 +1,162 @@
 const WhirlBlogs = () => {
     return (
-        <h1>Blogs</h1>
+        <section
+            style={{width:'1440px', height:'747px', backgroundColor:'#F7F9FA'}}
+        >
+            <div
+                style={{width:'1230px', height:'587px', left:'105px', top:'80px', margin:'auto'}}
+            >
+                <div
+                    style={{width:'1130px', height:'68px', top:'0.36px', left:'50px',
+                        display:"inline-flex", justifyContent:'space-between', margin:'32px auto', padding:'0px',
+                        marginBottom: '45px'
+                    }}
+                >
+                    <div
+                        style={{width:'533px', height:'51px', top:'-1.36px',
+                            fontSize:'42px', fontFamily:'Fraunces'
+                        }}
+                    >
+                        Get smarter, with our blog.
+                    </div>
+                    <div
+                        style={{width:'174px', height:'43px', top:'6.64px', color:'#0070A0', 
+                            textAlign:'center', paddingTop:'15px', fontSize:'17px',
+                            lineHeight:'27px', letterSpacing:'0.2px', 
+                        }}
+                    >
+                        See All Posts
+                    </div>
+                </div>
+                <div
+                    style={{width:'1130px', height:'486.625px', top:'100.36px', left:'50px',
+                        display:"inline-flex", justifyContent:'space-between', 
+                        margin:'auto'
+                    }}
+                >
+                    <div
+                        style={{width:'355px', height:'487', top:'-0.36px', backgroundColor:'#FFFFFF'}}
+                    >
+                        <img src="/images/IMAGE-5.png"
+                            style={{border:'none', borderRadius:'0px',
+                                width:'355px', height:'238px'
+                            }}
+                        />
+                        <div
+                            style={{width:'355.328125px', height:'249px', top:'237.98px', 
+                                textAlign:'left', paddingLeft:'8px'}}
+                        >
+                            <div 
+                                style={{backgroundColor:'#E6F7FF', color:'#2C90C9',
+                                    width:'122px', height:'22px', paddingTop:'2px',
+                                    fontSize:'12px', lineHeight:'16px', letterSpacing:'0.4px', textAlign:'center'
+                                }}
+                            >
+                                Improvements
+                            </div>
+                            <p
+                                style={{width:'293px', height:'63px', top:'55.02px',
+                                    fontSize:'25px', fontFamily:'Fraunces',
+                                    lineHeight:'31.25px', letterSpacing:'0.2px'
+                                }}
+                            >
+                                Automating Daily Tasks from Your Phone
+                            </p>
+                            <p
+                                style={{fontSize:'14px', lineHeight:'19.6px', letterSpacing:'0.2px'}}
+                            >
+                                Dicta nihil ratione corrupti. Aut dolorem dolores omnis laboriosam ratione sequi. Provident ad sed velit. Est ea ab.
+                            </p>
+                            <p
+                                style={{fontSize:'12px', lineHeight:'19.6px', letterSpacing:'0.2px'}}
+                            >
+                                April 24, 2022
+                            </p>
+                        </div>
+                    </div>
+                    <div
+                        style={{width:'355px', height:'487', top:'-0.36px', backgroundColor:'#FFFFFF', left:'387px'}}
+                    >
+                        <img src="/images/IMAGE-6.png"
+                            style={{border:'none', borderRadius:'0px',
+                                width:'355px', height:'238px'
+                            }}
+                        />
+                        <div
+                            style={{width:'355.328125px', height:'249px', top:'237.98px', 
+                                textAlign:'left', paddingLeft:'8px'}}
+                        >
+                            <div 
+                                style={{backgroundColor:'#E6F7FF', color:'#2C90C9',
+                                    width:'112px', height:'22px', paddingTop:'2px',
+                                    fontSize:'12px', lineHeight:'16px', letterSpacing:'0.4px', textAlign:'center'
+                                }}
+                            >
+                                Tips & Tricks
+                            </div>
+                            <p
+                                style={{width:'293px', height:'63px', top:'55.02px',
+                                    fontSize:'25px', fontFamily:'Fraunces',
+                                    lineHeight:'31.25px', letterSpacing:'0.2px'
+                                }}
+                            >
+                                Can You Automate Group Learning?
+                            </p>
+                            <p
+                                style={{fontSize:'14px', lineHeight:'19.6px', letterSpacing:'0.2px'}}
+                            >
+                                Dicta nihil ratione corrupti. Aut dolorem dolores omnis laboriosam ratione sequi. Provident ad sed velit. Est ea ab.
+                            </p>
+                            <p
+                                style={{fontSize:'12px', lineHeight:'19.6px', letterSpacing:'0.2px'}}
+                            >
+                                April 24, 2022
+                            </p>
+                        </div>
+                    </div>
+                    <div
+                        style={{width:'355px', height:'487', top:'-0.36px', backgroundColor:'#FFFFFF', left:'775px'}}
+                    >
+                        <img src="/images/IMAGE-7.png"
+                            style={{border:'none', borderRadius:'0px',
+                                width:'355px', height:'238px'
+                            }}
+                        />
+                        <div
+                            style={{width:'355.328125px', height:'249px', top:'237.98px', 
+                                textAlign:'left', paddingLeft:'8px'}}
+                        >
+                            <div 
+                                style={{backgroundColor:'#E6F7FF', color:'#2C90C9',
+                                    width:'60px', height:'22px', paddingTop:'2px',
+                                    fontSize:'12px', lineHeight:'16px', letterSpacing:'0.4px', textAlign:'center'
+                                }}
+                            >
+                                News
+                            </div>
+                            <p
+                                style={{width:'293px', height:'63px', top:'55.02px',
+                                    fontSize:'25px', fontFamily:'Fraunces',
+                                    lineHeight:'31.25px', letterSpacing:'0.2px'
+                                }}
+                            >
+                                Our $3,000,000 B Round Investors
+                            </p>
+                            <p
+                                style={{fontSize:'14px', lineHeight:'19.6px', letterSpacing:'0.2px'}}
+                            >
+                                Eos ipsum et est quis neque cum. Quis autem est eligendi amet animi eaque. Itaque minus illo delectus vel vitae dolores minus.
+                            </p>
+                            <p
+                                style={{fontSize:'12px', lineHeight:'19.6px', letterSpacing:'0.2px'}}
+                            >
+                                April 24, 2022
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
--- src/components/whirl/WhirlTasks.tsx
+++ src/components/whirl/WhirlTasks.tsx
@@ -1,6 +1,23 @@
 const WhirlTasks = () => {
     return (
-        <h1>Tasks</h1>
+        <section
+            style={{width:'1230px', height:'408px', margin:'32px auto', left:'105px', padding:'auto'}}
+        >
+            <div
+                style={{width:'1130px', height:'408px', left:'50px', top:'0.36px', 
+                    textAlign:'left', padding:'auto', margin:'auto'}}
+            >
+                <div
+                    style={{width:'650px', height:'122px', textAlign:'left'}}
+                >
+                    <p
+                        style={{fontSize:'42px', fontFamily:'Fraunces', lineHeight:'50.4px', letterSpacing:"0.2px"}}
+                    >
+                        Your tasks, automated.
+                    </p>
+                </div>
+            </div>
+        </section>
     )
 }
 
